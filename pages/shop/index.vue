66cef8e02a458b60d4c441d78f017ac4e3bc6845
Antek Karlsson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2023-04-05 21:11:59 +0200

    [WB-8] Update shop-page & product-card layout (#8)

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div v-if="store.allProducts">
-    <h1>SHOP</h1>
-    <div v-for="item in store.allProducts" :key="item.id">{{ item.name }}</div>
+  <div v-show="store.allProducts" class="shop-page">
+    <ProductCard
+      v-for="(product, index) in store.allProducts"
+      :key="index"
+      class="shop-page__card"
+      :product="product"
+    />
   </div>
 </template>
 
@@ -15,13 +19,43 @@ onMounted(() => {
 });
 </script>
 
-<style scoped>
-div {
+<style lang="scss" scoped>
+.shop-page {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 1rem;
   font-size: 3rem;
+  padding: 2.4rem;
+
+  @include tablet {
+    flex-direction: row;
+    justify-content: center;
+    flex-wrap: wrap;
+    padding: 1.2rem;
+  }
+
+  @include tablet-landscape {
+    padding: 4.8rem;
+  }
+
+  @include desktop {
+    padding: 6.4rem;
+    gap: 2rem;
+  }
+
+  &__card {
+    @include tablet {
+      max-width: 50%;
+    }
+
+    @include desktop {
+      max-width: 30%;
+    }
+
+    @include desktop-lg {
+      max-width: 20%;
+    }
+  }
 }
 </style>
