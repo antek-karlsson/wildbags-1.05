99f88a872a1325eb68eb39055e497616fa21c99b
Antek Karlsson <45142969+antek-karlsson@users.noreply.github.com>
2023-06-11 12:32:07 +0200

    Feat/wb 12 features section (#17) (#18)
    * [WB-12] Remove obsolete vite option from nuxt.config.ts
    
    * [WB-12] Update yarn.lock
    
    * [WB-12] Add static texts & images for Features.vue section
    
    * [WB-12] Create Features section related components:
    - FeatureCard.vue
    - Features.vue
    
    * [WB-12] Include Features.vue section in pages/index.vue

---
+++ api/data/static/features.ts
@@ -0,0 +1,22 @@
+export const features = [
+  {
+    id: 1,
+    title: 'JESTEŚMY ECO',
+    img: 'features_1.jpg',
+  },
+  {
+    id: 1,
+    title: 'WYŻSZA FORMA UPCYCLINGU',
+    img: 'features_2.jpg',
+  },
+  {
+    id: 1,
+    title: 'SLOW FASHION',
+    img: 'features_3.jpg',
+  },
+  {
+    id: 1,
+    title: 'ZERO WASTE',
+    img: 'features_4.jpg',
+  },
+];
