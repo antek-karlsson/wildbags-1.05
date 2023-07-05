c5fe076f2fb1f1b58a9d5286cef2a745c57099bd
Antek <tonylearnscoding@gmail.com>
2023-07-05 17:45:19 +0200

    [WB-15] Update content in: - about.ts - features.ts

--- api/data/static/about.ts
+++ api/data/static/about.ts
@@ -1,18 +1,22 @@
 export const about = [
   `
     Nie będę Ciebie zanudzać historyjkami o marzeniach z dzieciństwa i pasji do szycia czy wzornictwa... :)
-    Swoją pierwszą maszynę kupiłam 3 lata temu i nie wiedziałam nawet jak zamontować igłę... Wiele ( naprawdę wiele) godzin dni, miesięcy spędziłam na nauce z czym to się w ogóle je. Prawda jest taka, że wszystkiego uczyłam się sama z miliona filmików na zagranicznym YouTubie i stron w obcych językach ( w tym koreańskim! ) Teraz śmiało mogę powiedzieć, że zaprzyjaźniłam się z moją maszyną i umiem rozpoznawać jej fochy :)
   `,
   `
-    Szyję dla Ciebie (i dla siebie też!) rzeczy z upcyklingu. Szyje ze wszystkiego co mam aktualnie pod ręką… Raz jest to ZASŁONA, którą przerobię na torbę czy kosmetyczkę, a niekiedy są to dywaniki, bluzki, spódniczki, sukienki, szaliki które znajdę w SECOND HANDZIE lub dostanę w spadku po porządkach w szafie czyjejś babci, mamy, koleżanki – każdego kto chce mi oddać COŚ co na pewno pójdzie na śmietnik.
+    Wszystko co widzisz w moim sklepie i na instagramie jest owocem mojej największej pasji - czyli szycia.
+    Wszystkie produkty powstają z unikatowych tkanin z całego świata. Poszukiwanie tych tkanin jest praktycznie moją drugą pracą na etacie :D
   `,
   `
-    W wieku 17-18 lat praktycznie zrezygnowałam na dłuższą metę z kupna w sieciówkach  i chcąc nie chcąc – świadomie lub nie, przestałam wspierać postępujący konsumpcjonizm. Ubrania z drugiej ręki , tak samo jak te z drogich luksusowych sklepów mogą służyć nam dłuuuuugie lata.
+    Śmiało mogę pokusić się o stwierdzenie, że takich rzeczy jakie znajdziesz u mnie - nie znajdziesz nigdzie indziej :)
   `,
   `
-    BA! Mogę się pokusić nawet o stwierdzenie, że wszystko co z drugiej ręki jest fajniejsze!
+    Niektóre produkty są szyte z tkanin z drugiej ręki - czyli w duchu ZERO WASTE.
+    Jak tylko uda mi się "upolować" fajną tkaninę z lumpeksu, albo z szafy czyjejś babci - od razu idzie pod igłę i daje jej nowe życie!
   `,
   `
-    Bardzo cieszy mnie fakt, że mogę przerobić niepotrzebne już komuś wyroby na niepowtarzalne i wyjątkowe produkty ZERO WASTE a przy okazji dbam o naszą Mamę Ziemię
+    Jestem dumna z dostarczania innym kobietom ( chłopakom też, ale dużo rzadziej ) wysokiej jakości produktów, które łączą modę, funkcjonalność i trwałość :)
+  `,
+  `
+    Mam nadzieje, że znajdziesz u mnie idealny prezent dla przyjaciółki albo perfekcyjną kosmetyczkę na niezbędne Ci drobiazgi <3
   `,
 ];
